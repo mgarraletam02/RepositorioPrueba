26a781e6256c4cbf13c95cac43538f08ece73917
mgarraletam01 <Miguel@PC1>
2018-05-22 17:56:32 +0200

    La primera modificación, a cargo de mgarraletam01

--- HolaGitHub/src/Hola.java
+++ HolaGitHub/src/Hola.java
@@ -3,6 +3,6 @@ public class Hola {
 
 	public static void main(String[] args) {
 		System.out.println("Primera versión del código. Proyecto para Github");
+		System.out.println("El usuario mgarraletam02 modifica el proyecto");
 	}
-
 }
