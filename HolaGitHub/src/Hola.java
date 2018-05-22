cbcdf726d50bc010d92330df9496ddd27b199dc1
mgarraletam01 <Miguel@PC1>
2018-05-22 18:48:15 +0200

    Primera modificación de la primera rama

--- HolaGitHub/src/Hola.java
+++ HolaGitHub/src/Hola.java
@@ -4,5 +4,6 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Primera versión del código. Proyecto para Github");
 		System.out.println("El usuario mgarraletam02 modifica el proyecto");
+		System.out.println("Línea añadida en la primera rama");
 	}
 }
